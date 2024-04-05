33a36a8f70c066f648ebeb445945a5a66f53a374
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-05 16:44:21 -0300

    exercicio

---
+++ 5_op_expressao/7_op_modulo/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo 12 % 2;
+    echo " Modulo <br>";
+
+    echo 12 / 2;
+    echo " Divisão <br>";
+    
---
+++ 5_op_expressao/8_exercicio_13/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $a = 14;
+    $b = 2;
+    $c = 3;
+
+    echo $a % $b;
+    echo " Modulo <br>";
+
+    echo $a % $c;
+    echo " Modulo <br>";
