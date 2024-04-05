923bd459711e3afaf0146a903b6994dff89d5eba
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-05 16:23:19 -0300

    op aritimetrico

---
+++ 5_op_expressao/5_op_aritimetico/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $soma = 5 + 5;
+    echo $soma . "<br>";
+
+    $subtracao = 15 - 9;
+    echo $subtracao . "<br>";
+
+    $mult = 5 * 5;
+    echo $mult . "<br>";
+
+    $div = 10 / 5;
+    echo $div . "<br>";
