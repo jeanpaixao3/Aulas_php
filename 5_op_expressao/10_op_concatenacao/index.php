e5fe80d566a88f5d979fd00acc708c65d6abdac6
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-05 16:59:49 -0300

    concatenação

---
+++ 5_op_expressao/10_op_concatenacao/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "testanto a concatenação";
+    echo "<br>";
+
+    echo "testando" . " a " . "concatenação";
+    echo "<br>";
+
+    $t = "testanto";
+    $c = "concatenação";
+
+    echo $t . " a " . $c;
+
+    echo "<br>";
+    $marca = "Renault";
+    $carro = 'Duster';
+    $vel_max = 200;
+
+    echo " O Carro da " . $marca . " modelo " . $carro . "chega a velocidade maxima de " . $vel_max . " km/h";
