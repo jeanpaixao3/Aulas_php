bd531ee8aedffcc387298e907db83ec9cc9f1f8a
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-05 16:30:11 -0300

    exercio 12

---
+++ 5_op_expressao/6_exercicio_12/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $x = 50;
+    $y = 45;
+    
+    $soma = $x + $y;
+    echo "Soma: $soma  <br>";
+
+    $mult = $x * $y;
+    echo "Mult: $mult  <br>";
+
+    $div = $x / $y;
+    echo "Div: $div  <br>";
+
+    $sub = $x - $y;
+    echo "Sub: $sub  <br>";
