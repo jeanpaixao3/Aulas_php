a6dc6531111364dad8c1b15baffe6dbe99689cb1
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-11 11:56:33 -0300

    exercio 15

---
+++ 5_op_expressao/14_exercicio_15/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a = 12;
+    $b = 12;
+    $c = 100;
+
+    if($a == $b) {
+        echo ("comparação verdadeira <br>");
+    }
+
+    if($a == $c) {
+        echo ("comparação verdadeira <br>");
+    }
+
+
