e350ad3fcfa1dc9b542e6603a1b463abf66c8038
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-11 11:51:30 -0300

    operação de iguldade

---
+++ 5_op_expressao/13_op_igualdade/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    if(3 == 3) {
+        echo ("Comparação verdadeira <br>");
+    }
+
+    if(3 == 4) {
+        echo("Comparação verdadeira <br>");
+    }
+
+    $a = 12;
+    $b = 12;
+    $c = 100;
+
+    if($a == $b) {
+        echo ("comparação verdadeira <br>");
+    }
+
+    if($a == $c) {
+        echo ("comparação verdadeira <br>");
+    }
+
+
