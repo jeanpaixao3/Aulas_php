0af58c9f0e56ad208c507e2a17de335148847ffc
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 14:05:53 -0300

    exercicio 23

---
+++ 6_ESTRUTURA _DE_CONTROLE/5_exercicio_23/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$idade1 = 18;
+$idade2 = 17;
+$maiorIdade = 18;
+$msg = "Você é maior idade<br>";
+
+if($idade1 >= $maiorIdade) {
+    echo $msg;   
+} else {
+    echo "Você é menor idade <br>";
+}
+
+
+if($idade2 >= $maiorIdade) {
+    echo $msg;
+   
+} else {
+    echo "Você é menor idade";
+}
