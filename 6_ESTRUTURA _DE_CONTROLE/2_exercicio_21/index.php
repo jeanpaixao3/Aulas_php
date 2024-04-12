f61901a0c6d860ca50628c9bee0de3b4648a5179
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 13:46:17 -0300

    exercio 21

---
+++ 6_ESTRUTURA _DE_CONTROLE/2_exercicio_21/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    if(5 > 2) {
+        echo "5 é maior <br>";
+    }
+
+    if("Matheus" != "Pedro") {
+        echo "É diferente <br>";
+    }
+
+    if(12 <= 11) {
+        echo "verdade";
+    }
