70fe4e7ae9f76fa27919023b37c4488a045c7734
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 13:41:46 -0300

    estrura de  controle

---
+++ 5_op_expressao/30_exercio 20/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $a = 6;
+    $b = 10;
+
+    //true
+    echo 20 <= 10 ? "Deu true <br>" : "Deus falso <br>";
+
+    //falso
+    echo 200 <= 1000 ? "Deu true <br>" : "Deus falso <br>";
---
+++ 6_ESTRUTURA _DE_CONTROLE/1_estrutura_if/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    if(20 > 5) {
+        echo "Deus certo entro no if 1 <br>";
+    }
+
+    if(2 < 5) {
+        echo "Deus certo entro no if 2 <br>";
+    }
+
+
