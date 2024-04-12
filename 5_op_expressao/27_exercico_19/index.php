8df3ebc48e789e134fb82bc126370e7ba66b265b
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 11:44:42 -0300

    exercioco 19

---
+++ 5_op_expressao/27_exercico_19/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $a = (int) "testando";
+    echo $a . "<br>";
+
+    $a = (int) "asd";
+    echo $a . "<br>";
+
+    $c = (int) 12.5;
+    echo $c . "<br>";
+
+    $d = (int) true;
+    echo $d . "<br>";
+
+    $e = (int) false;
+    echo $e . "<br>";
+
+    $g = (int) [1,2,3];
+    echo $g . "<br>";
