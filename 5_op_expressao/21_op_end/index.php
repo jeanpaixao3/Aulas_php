0f4840dfc595406c3b5f6f9799ba9392d8643334
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 11:02:54 -0300

    operador END

--- 5_op_expressao/21_op_end/index.php
+++ 5_op_expressao/21_op_end/index.php
@@ -1,3 +1,15 @@
 <?php
 
-    
+    if(5 > 4 && 66 > 40) {
+        echo "Entre no if 1 <br>";
+    }
+
+    if(5 < 40 && 66 > 40) {
+        echo "Entre no if 2 <br>";
+    }
+
+    if(5 >= 4 && 66 >= 66) {
+        echo "Entre no if 3 <br>";
+    }
+
+
