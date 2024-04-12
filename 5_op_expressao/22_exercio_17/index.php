2a07d7c2d6a022aebaeab2af47d068afb4cd4b58
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 11:10:12 -0300

    exercicio 17

---
+++ 5_op_expressao/22_exercio_17/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    if(15 > 5 && "João" === "João") {
+        echo "Entre no if 1 <br>";
+    }
+
+    if("teste" > 5 && 1) {
+        echo "Entre no if 2 <br>";
+    }
+
+    if(2 == 3 && 5 >=3) {
+        echo "Entre no if 3 <br>";
+    }
+
+
+
+    
