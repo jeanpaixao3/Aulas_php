d566e12a015ff73242958c4c5d04fcdb893042ea
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-12 11:20:23 -0300

    exercio 18

---
+++ 5_op_expressao/24_exercicio_18/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    if(12 < 5 || "joão" === "joão") {
+        echo "Operação 1 é verdadeira <br>";
+    }
+
+    if(1 > 5 || 1) {
+        echo "Operação 2 é verdadeira <br>";
+    }
+
+    if(20 === "20" || 51 >= 31 ) {
+        echo "Operação 3 é verdadeira";
+    }
