6dbd6e96bfeac44dd3761693fd55f7dfb0537011
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-17 09:23:57 -0300

    while

---
+++ 7_estrutura_de_repiticao/1-while/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // A definição do contador
+    $x = 0;
+
+    // Inicio / Definição da estrutura
+    while($x <10) {
+        // Corpo do loop
+        echo $x ."<br>";
+
+        // Incremento do contador
+        $x++;
+    }
