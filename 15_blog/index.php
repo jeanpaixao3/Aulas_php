65b9145fbc30a7614bcbce16fb7c8f4a85ee1314
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-18 10:01:42 -0300

    crinado blog

--- 15_blog/index.php
+++ 15_blog/index.php
@@ -1,16 +1,32 @@
 <?php
-    include_once("helpers/url.php");
+    include_once("templates/header.php");
 ?>
+    <main>
+        <div id="title-container">
+            <h1>Blog Codando</h1>
+            <p>O seu blog de programação</p>
+        </div>
+        <div id="posts-container">
+            <?php foreach($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                    <h2 class="post-title">
+                        <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+                    </h2>
+                    <p class="post-description"><?= $post['description']?></p>
+                    <div class="tags-container">
+                    <?php foreach($post['tags'] as $tag): ?>
+                        <a href="#"><?= $tag ?></a>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Codando</title>
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
-</head>
-<body>
-    
-</body>
-</html>
+                    <?php endforeach?>
+                        
+                    </div>
+
+
+                </div>
+            <?php endforeach?>
+        </div>
+    </main>
+<?php
+    include_once("templates/footer.php")
+?>
